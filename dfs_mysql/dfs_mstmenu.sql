760efa7007f230468d45cd86e23db8a4a2231014
Berry10 <30311360+Berry10@users.noreply.github.com>
2017-08-14 07:47:02 +0530

    DFS database structure
    Daksham mysql database.

---
+++ dfs_mysql/dfs_mstmenu.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE  IF NOT EXISTS `dfs` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `dfs`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dfs
+-- ------------------------------------------------------
+-- Server version	5.6.36-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `mstmenu`
+--
+
+DROP TABLE IF EXISTS `mstmenu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mstmenu` (
+  `menuID` int(11) NOT NULL AUTO_INCREMENT,
+  `menuLabel` varchar(100) NOT NULL,
+  `menuDesc` varchar(255) NOT NULL,
+  `iconFileName` varchar(255) DEFAULT NULL,
+  `menuLinkURL` varchar(255) DEFAULT NULL,
+  `parentMenuID` int(11) NOT NULL,
+  `creationDate` datetime NOT NULL,
+  `actionDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `actionUserID` int(11) NOT NULL,
+  PRIMARY KEY (`menuID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mstmenu`
+--
+
+LOCK TABLES `mstmenu` WRITE;
+/*!40000 ALTER TABLE `mstmenu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mstmenu` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-08-14  7:43:05
